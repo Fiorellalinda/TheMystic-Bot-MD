e9ebd87360ec54b5a860b7fe5d2081f0a0cd6853
Fiorellalinda <fiorellaparedes000@gmail.com>
2024-08-28 23:54:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from 'url';
 import fs from 'fs'; 
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "543704786161"
 global.confirmCode = ""
 global.authFile = `MysticSession`;
 
